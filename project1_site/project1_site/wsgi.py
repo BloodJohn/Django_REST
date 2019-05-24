9bc346d2bc0db85106a1b52be369f19b37e3217c
Eugene Pavlov <bjohn.ext@gmail.com>
2019-05-24 10:20:32 +0300

    upd

--- project1_site/project1_site/wsgi.py
+++ project1_site/project1_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_rest2 project.
+WSGI config for project1_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1_site.settings')
 
 application = get_wsgi_application()
