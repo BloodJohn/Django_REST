4292e6a43b85e358f2b4c6cf5396208dd4630fde
Eugene Pavlov <bjohn.ext@gmail.com>
2019-05-28 11:58:54 +0300

    add blog

--- project1_site/project1_site/urls.py
+++ project1_site/project1_site/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
-from django.conf.urls.static import static
 from django.urls import path, include
-
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('quickstart.urls')),
+#    path('accounts/login/', views.LoginView.as_view(), name='login'),
+#    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('', include('blog.urls')),
+
 ]
 
 # if settings.DEBUG is True:
