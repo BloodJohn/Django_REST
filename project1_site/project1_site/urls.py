3dbdc959eab6f1ea9bffdd5c299fe140277061f9
Eugene Pavlov <bjohn.ext@gmail.com>
2019-05-28 12:12:34 +0300

    upd

--- project1_site/project1_site/urls.py
+++ project1_site/project1_site/urls.py
@@ -20,8 +20,8 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('quickstart.urls')),
-#    path('accounts/login/', views.LoginView.as_view(), name='login'),
-#    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
 
 ]
