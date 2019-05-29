7137dcb3b1464a47bbf11452973f07e5197c69b8
Eugene Pavlov <bjohn.ext@gmail.com>
2019-05-29 15:41:01 +0300

    upd

--- project1_site/blog/apiViews.py
+++ project1_site/blog/apiViews.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from django.utils import timezone
+from rest_framework.response import Response
 
 from blog.models import Post
 from blog.serializers import PostSerializer
@@ -11,3 +12,10 @@ class BlogList(viewsets.ModelViewSet):
     """
     queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     serializer_class = PostSerializer
+
+    # возвращает json по http запросу
+    def list(self, request, *args, **kwargs):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
